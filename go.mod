27eb13f009f97f24ada51b2370f039a069797482
= <sharukhprogramming@gmail.com>
2025-05-14 22:08:14 +0530

    added config file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sharukh010/user-managment
 
 go 1.24.2
 
-require gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
---
+++ pkg/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gopkg.in/mgo.v2"
+)
+
+func getSession() *mgo.Session {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error occured while loading env")
+	}
+	connString := os.Getenv("MONGODB_URL")
+	s,err := mgo.Dial(connString)
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
